3abfb5e6f7b7b96a7f6a0f278be4dedcc5d93d95
Doug Puchalski <doug316@gmail.com>
2012-09-29 10:45:28 -0700

    Initial commit based on https://gist.github.com/3704111

---
+++ Rakefile
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+$:.unshift("/Library/RubyMotion/lib")
+require 'motion/project'
+
+Motion::Project::App.setup do |app|
+  # Use `rake config' to see complete project settings.
+  app.name = 'HelloMailCore'
+
+  # Configure MailCore
+  app.vendor_project('vendor/MailCore',
+    :xcode, :scheme => "MailCore iOS", :headers_dir => '.')
+  app.frameworks << "CFNetwork"
+  app.libs << "/usr/lib/libssl.dylib"
+  app.libs << "/usr/lib/libiconv.dylib"
+  app.libs << "/usr/lib/libsasl2.dylib"
+  app.libs << "/usr/lib/libcrypto.dylib"  
+end
---
+++ app/app_delegate.rb
@@ -0,0 +1,22 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    msg = CTCoreMessage.alloc.init
+    msg.setTo(NSSet.setWithObject(CTCoreAddress.addressWithName("Someone", email:"someone@example.com")))
+    msg.setFrom(NSSet.setWithObject(CTCoreAddress.addressWithName("Watson", email:"watson1978@gmail.com")))
+    msg.setBody("This is a test message!")
+    msg.setSubject("This is a subject")
+
+    error = Pointer.new(:id)
+    ret = CTSMTPConnection.sendMessage(msg,
+                                       server:"smtp.gmail.com",
+                                       username:"xxxxxxxx@gmail.com",
+                                       password:"xxxxxxxx",
+                                       port:587,
+                                       useTLS:true,
+                                       useAuth:true,
+                                       error:error)
+    true
+
+  end
+end
