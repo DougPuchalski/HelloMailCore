9868a8cec6a9d8087abb2a70f1d3c937cc8013f2
Doug Puchalski <doug316@gmail.com>
2012-09-30 09:34:52 -0700

    Prove that a couple of things are defined

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
+    # The following should be defined!
+    puts "::CTConnectionTypeTLS #{::CTConnectionTypeTLS}"
+    puts "::CONNECTION_TYPE_TLS #{::CONNECTION_TYPE_TLS}"
+
     msg = CTCoreMessage.alloc.init
     msg.setTo(NSSet.setWithObject(CTCoreAddress.addressWithName("Someone", email:"someone@example.com")))
     msg.setFrom(NSSet.setWithObject(CTCoreAddress.addressWithName("Watson", email:"watson1978@gmail.com")))
